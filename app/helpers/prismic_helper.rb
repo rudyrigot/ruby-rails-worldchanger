5a131a21e4376e3ac56d5333abf931b1bf4b9349
Rudy Rigot <rudy.rigot@gmail.com>
2013-11-22 15:12:32 -0800

    Link resolving for home, tour, and arguments

--- app/helpers/prismic_helper.rb
+++ app/helpers/prismic_helper.rb
@@ -12,7 +12,19 @@ module PrismicHelper
   end
 
   def link_resolver(ref)
-    Prismic::LinkResolver.new(ref){|doc| document_path(id: doc.id, slug: doc.slug) }
+    Prismic::LinkResolver.new(ref) do |doc|
+      case doc.link_type
+      when "homepage"
+        root_path
+      when "article"
+        tour_path if doc.id == api.bookmark("tour")
+      when "argument"
+        tour_path + "#" + doc.id
+      else
+        # I'm leaving this here as long as I'm not done developing all the templates, it gives me a default one
+        document_path(id: doc.id, slug: doc.slug)
+      end
+    end
   end
 
   def privileged_access?
