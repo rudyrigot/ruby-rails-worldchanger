edeab5b8b8ffaa2a7013936f4580251208a95d01
Rudy Rigot <rudy.rigot@gmail.com>
2013-12-03 12:36:22 -0800

    Fixing the orderings, now that they are in the API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,56 +2,50 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_ref
 
-=begin
-	In this controller, orderings are passed as a second parameter of create_search_form.
-	This is a tweak while waiting for the orderings to be officially added in the API forms,
-	you should NEVER do that in production code, as it is a hack and is not guaranteed
-	to work forever.
-	Rather, the proper method will be to pass the orderings in the `ordering` method
-	to apply on the search form, that doesn't exist yet. It will look like this:
-
-	api.create_search_form("arguments")
-       .query(%([[:d = at(document.tags, ["userfriendly"])][:d = at(document.tags, ["featured"])]]))
-       .orderings("[my.argument.priority desc]")
-       .submit(@ref)
-=end
-
   def index
   	@document = PrismicService.get_document(api.bookmark("homepage"), api, @ref)
-    @user_friendly_arguments = api.create_search_form("arguments", {"orderings" => "[my.argument.priority desc]"})
+    @user_friendly_arguments = api.create_search_form("arguments")
                     .query(%([[:d = at(document.tags, ["userfriendly"])][:d = at(document.tags, ["featured"])]]))
+                    .set("orderings", "[my.argument.priority desc]")
                     .submit(@ref)
-    @design_arguments = api.create_search_form("arguments", {"orderings" => "[my.argument.priority desc]"})
+    @design_arguments = api.create_search_form("arguments")
                     .query(%([[:d = at(document.tags, ["design"])][:d = at(document.tags, ["featured"])]]))
+                    .set("orderings", "[my.argument.priority desc]")
                     .submit(@ref)
-    @minimum_price = api.create_search_form("plans", {"orderings" => "[my.pricing.price]"}).submit(@ref)[0]
+    @minimum_price = api.create_search_form("plans")
+                    .set("orderings", "[my.pricing.price]").submit(@ref)[0]
                     .fragments['price'].value.to_i
-    @questions = api.create_search_form("questions", {"orderings" => "[my.faq.priority desc]"})
+    @questions = api.create_search_form("questions")
                     .query(%([[:d = at(document.tags, ["featured"])]]))
+                    .set("orderings", "[my.faq.priority desc]")
                     .submit(@ref)
   end
 
   def tour
     @document = PrismicService.get_document(api.bookmark("tour"), api, @ref)
-    @arguments = api.create_search_form("arguments", {"orderings" => "[my.argument.priority desc]"})
+    @arguments = api.create_search_form("arguments")
+                    .set("orderings", "[my.argument.priority desc]")
                     .submit(@ref)
   end
 
   def pricing
     @document = PrismicService.get_document(api.bookmark("pricing"), api, @ref)
-    @plans = api.create_search_form("plans", {"orderings" => "[my.pricing.price]"})
+    @plans = api.create_search_form("plans")
+                    .set("orderings", "[my.pricing.price]")
                     .submit(@ref)
   end
 
   def about
   	@document = PrismicService.get_document(api.bookmark("about"), api, @ref)
-  	@staff = api.create_search_form("staff", {"orderings" => "[my.author.level]"})
+  	@staff = api.create_search_form("staff")
+                    .set("orderings", "[my.author.level]")
                     .submit(@ref)
   end
 
   def faq
     @document = PrismicService.get_document(api.bookmark("faq"), api, @ref)
-    @questions = api.create_search_form("questions", {"orderings" => "[my.faq.priority desc]"})
+    @questions = api.create_search_form("questions")
+                    .set("orderings", "[my.faq.priority desc]")
                     .submit(@ref)
   end
 
